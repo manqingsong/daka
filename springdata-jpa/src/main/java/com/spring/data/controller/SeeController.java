63a72eb2aab4ba176f977887ab4321bc1c5ccc69
满青松 <137438350@qq.com>
2019-04-10 19:36:47 +0800

    提交see controller

---
+++ springdata-jpa/src/main/java/com/spring/data/controller/SeeController.java
@@ -0,0 +1,13 @@
+package com.spring.data.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Date: 2019/04/10
+ * Time: 下午19:34
+ *
+ * @author manqs
+ */
+@RestController
+public class SeeController {
+}
