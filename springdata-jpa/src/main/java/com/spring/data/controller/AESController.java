4dde9278406f32e5f5ed58b247ef6cebd50df296
满青松 <137438350@qq.com>
2019-05-05 22:52:28 +0800

    修改返回值的类型

--- springdata-jpa/src/main/java/com/spring/data/controller/AESController.java
+++ springdata-jpa/src/main/java/com/spring/data/controller/AESController.java
@@ -18,8 +18,8 @@ public class AESController {
     public String encodeAes(@RequestBody String str){
         if (StringUtils.isNotBlank(str)) {
             // 修改返回josn类型
-            String aesEncode = AESUtils.AESEncode(AESUtils.key, str);
-            return JSON.toJSONString(aesEncode);
+            String result = AESUtils.AESEncode(AESUtils.key, str);
+            return JSON.toJSONString(result);
         }
         // 增加日志
         System.out.println(str);
